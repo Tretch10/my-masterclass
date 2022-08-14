47d7a0c0100ff8efa2295a69e4e9925cf6ed46d3
Tretch10 <idenupeter@yahoo.com>
2022-08-14 00:02:58 -0500

    array as list

--- src/masterclass/Testing.java
+++ src/masterclass/Testing.java
@@ -1,5 +1,15 @@
 package masterclass;
 
-public interface Testing {
-    public static int test1=6;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
+public class Testing {
+
+    static List<Integer> scores = Arrays.asList(12, 6, 7, 4, 8, 16);
+    public static void main(String[] args) {
+
+        scores.stream().sorted()
+                .forEach(System.out::println);
+    }
 }
